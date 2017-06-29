83a1262baeaec0ebebe5d737848eabb98f14e608
Christopher Kinch <Christopher_Kinch@dennis.co.uk>
2017-06-29 16:06:43 +0100

    Case 28112; Adding breakpoint functions

--- support/helpers.js
+++ support/helpers.js
@@ -1,13 +1,43 @@
-/**
- * Finds a nested object from it's parent using a dot notation string
- *
- * @params {Object} obj The object to look through
- * @params {string} str The dot notation string to find
- * @returns Object
- */
-module.exports.objRef = function(obj, str) {
-  str = str.split(".");
-  for (var i = 0; i < str.length; i++)
-  obj = obj[str[i]];
-  return obj;
-}
+var webdriver = require('selenium-webdriver');
+var store = {};
+    store.breakpoints = {};
+
+// extension method grab breakpoints from the homepage
+webdriver.WebDriver.prototype.getBreakpoints = function(name){
+  var driver = this;
+
+  return new Promise(function (resolve, reject){
+    if (store.breakpoints[name]) {
+      resolve(store.breakpoints);
+    } else {
+      driver.manage().timeouts().pageLoadTimeout(180 * 1000);
+      driver.get(driver.baseUrl);
+
+      driver.executeScript('return Drupal.settings.dennisJs.breakpoints').then(function(breakpoints){
+        var breakpoint = breakpoints[name];
+        store.breakpoints[name] = {};
+
+        if(breakpoint.match('max-width: (.*?)px')) {
+          store.breakpoints[name]['max-width'] = parseInt(breakpoint.match('max-width: (.*?)px')[1]);
+        }
+        if(breakpoint.match('min-width: (.*?)px')) {
+          store.breakpoints[name]['min-width'] = parseInt(breakpoint.match('min-width: (.*?)px')[1]);
+        }
+
+        resolve(store.breakpoints);
+      }, function(err) {
+        reject(err);
+      });
+    }
+  });
+};
+
+// extension method to set the inner size
+webdriver.WebDriver.prototype.setViewportSize = function(width, height){
+  return this.executeScript(JS_GET_PADDING).then(function(pad){
+    this.manage().window().setSize(parseInt(width), parseInt(height));
+  }.bind(this));
+};
+
+
+module.exports.store = store;
